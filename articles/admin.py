8d6c1ad821dc771dc7a17c8f78e4c9c347b3a845
ssuf <ssuf2002@gmail.com>
2023-12-16 12:31:18 -0400

    ayuda comments

--- articles/admin.py
+++ articles/admin.py
@@ -4,5 +4,11 @@ from django.contrib import admin
 
 from .models import Article, Comment
 
+class CommentInLine(admin.StackedInline):
+    model = Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInLine,]
+
 admin.site.register(Article)
 admin.site.register(Comment)
